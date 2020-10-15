1e0d52eedb4596aa24fe6f9664f422bf7b9ce68c
Sean Emery <semery@oxy.edu>
2020-10-14 22:42:06 -0700

    Updates

---
+++ precache-manifest.e2207765f0e7e4aa538ea0d6048c7269.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e07ea6356c2101b7c6b5234440287f3",
+    "url": "/react-search-json/index.html"
+  },
+  {
+    "revision": "d3c117130ea70d7fda8c",
+    "url": "/react-search-json/static/css/main.1e8b6bc0.chunk.css"
+  },
+  {
+    "revision": "4e8d2ed8e5775951b0d6",
+    "url": "/react-search-json/static/js/2.1b09de59.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-search-json/static/js/2.1b09de59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3c117130ea70d7fda8c",
+    "url": "/react-search-json/static/js/main.a8d9b346.chunk.js"
+  },
+  {
+    "revision": "a104d85b329cc19376f1",
+    "url": "/react-search-json/static/js/runtime-main.d2c8c473.js"
+  }
+]);
